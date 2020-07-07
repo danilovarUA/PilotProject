93c958eae73df4a218a12c2c4be244eef58c7735
Danilo <dan.varushchik@gmail.com>
2020-07-07 22:18:29 +0300

    a lot of stuff done

--- data.js
+++ data.js
@@ -1,12 +1,93 @@
 const tabs = [
-    {
-        id: 0,
-        text: "People",
-        icon: "group"
-    },
-    {
-        id: 1,
-        text: "Planets",
-        icon: "globe"
-    },
+    {id: 0, text: "People", icon: "group"},
+    {id: 1, text: "Planets", icon: "globe"},
 ];
+
+let people = [];
+let planets = [];
+let peopleFields = ["name", "height", "mass", "hair_color", "skin_color", "eye_color", "birth_year", "gender",
+    "homeworld", "films", "species", "vehicles", "starships", "created", "edited", "url"];
+let planetsFields = ["name", "rotation_period", "orbital_period", "diameter", "climate", "gravity", "terrain",
+    "surface_water", "population", "residents", "films", "created", "edited", "url"];
+let peopleLoaded = false;
+let planetsLoaded = false;
+let elementsData = {};
+
+function loadData(url, selector){
+    console.log("adding data for " + selector + " from url " + url)
+    $.get(url, function(data){
+        if (selector === "People"){
+            people = people.concat(data["results"])
+                // load single data
+                for (let human of data["results"]){
+                    loadSingleData(human["homeworld"])
+                    for (let film of human["films"]){
+                        loadSingleData(film)
+                    }
+
+                }
+                // the end
+        }
+        if (selector === "Planets"){
+            planets = planets.concat(data["results"])
+        }
+        if (data.next !== undefined && data.next !== null)
+        {
+            loadData(data.next, selector)
+        }
+        else {
+            if (selector === "People"){
+                peopleLoaded = true
+            }
+            if (selector === "Planets"){
+                planetsLoaded = true
+            }
+            if (peopleLoaded && planetsLoaded){
+                waitForSingleDataToLoad()
+            }
+        }
+    })
+}
+
+function loadSingleData(url) {
+    if (!(url in elementsData)){
+        console.log("adding data for " + url)
+        elementsData[url] = 0;
+        $.get(url, function (data) {
+            elementsData[url] = data
+        })
+    }
+}
+
+function waitForSingleDataToLoad(){
+    while (!allSingleDataLoaded())
+    {
+        console.log("Not all data loaded")
+    }
+    replaceLinksWithNames()
+}
+
+function allSingleDataLoaded(){
+    for (let element in elementsData){
+        if (elementsData[element] === 0){
+            console.log(element)
+            setTimeout(() => {  return false }, 200);
+        }
+    }
+    return true
+}
+
+function replaceLinksWithNames(){
+    for (let human of people){
+        human["homeworld"] = elementsData[human["homeworld"]]["name"]
+
+        for (let count = 0; count > human["films"].length; count++){
+            console.log("replaced " + uman["films"][count] + " with " + elementsData[human["films"][count]]["name"])
+            human["films"][count] = elementsData[human["films"][count]]["name"]
+        }
+    }
+    loadTable()
+}
+
+$(loadData("https://swapi.dev/api/people/", "People"))
+$(loadData("https://swapi.dev/api/planets/", "Planets"))
--- index.js
+++ index.js
@@ -1,6 +1,80 @@
+const SOURCE_TO_COLUMNS = {
+    "People": ["name", "height", "mass", "hair_color", "skin_color", "eye_color", "birth_year", "gender", "homeworld"],
+    "Planets": ["name", "rotation_period", "orbital_period", "diameter", "climate", "gravity", "terrain", "surface_water", "population"],
+}
+const PAGE_SIZES = [10, 50, 100]
+const DEFAULT_PAGE_SIZE = PAGE_SIZES[0]
+
+// Tabs
 $(function(){
     $("#tabs > .tabs-container").dxTabs({
         dataSource: tabs,
         selectedIndex: 0,
     }).dxTabs("instance");
 });
+
+// Table
+function loadTable() {
+    let value = $('.dx-tab-selected span').text();
+    console.log("loading table with " + value)
+    let source;
+    if (value === "People"){
+        source = people
+    }
+    else if (value === "Planets")
+    {
+        source = planets
+    }
+    $("#gridContainer").dxDataGrid({
+        dataSource: source,
+        columns: SOURCE_TO_COLUMNS[source],
+        showBorders: true,
+        paging: {
+            pageSize: DEFAULT_PAGE_SIZE
+        },
+        pager: {
+            showPageSizeSelector: true,
+            allowedPageSizes: PAGE_SIZES,
+            showInfo: true
+        },
+    });
+}
+
+let modalOpened = false
+function toggleModal(event){
+    let row;
+    console.log("toggling from " + modalOpened)
+    if (event !== undefined){
+        console.log(event.target.parentNode)
+        row = parseInt(event.target.parentNode.getAttribute("aria-rowindex")) - 1
+    }
+    // TODO: Why isn't it working with JQuery?
+    if (modalOpened) {
+        document.getElementById("modalDiv").style.display = "none";
+    }
+    else {
+        document.getElementById("modalDiv").style.display = "block";
+        loadModal(row);
+    }
+    modalOpened = !modalOpened
+}
+
+function loadModal(row){
+    let value = $('.dx-tab-selected span').text();
+    let source;
+    let fields;
+    if (value === "People"){
+        source = people
+        fields = peopleFields
+    }
+    else if (value === "Planets")
+    {
+        source = planets
+        fields = planetsFields
+    }
+    $("#form").dxForm({
+        colCount: 2,
+        formData: source[row],
+        items: fields
+    });
+}
